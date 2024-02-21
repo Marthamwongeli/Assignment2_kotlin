1b3a00c18e9378d52eac1bb225a36f6e07f7f765
Marthamwongeli <marthamutuaa@gmail.com>
2024-02-21 22:46:50 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+import org.w3c.dom.Text
+import kotlin.coroutines.coroutineContext
+
+fun main() {
+    greet("Mary")
+    modulus(56,15)
+    add(87,57,98,145)
+    myFunfact("I love drawing and painting")
+}
+fun greet(name:String){
+    var result= "Hello $name"
+    println(result)
+}
+fun modulus(num1:Int,num2:Int){
+    var result=num1%num2
+    println(result)
+}
+fun add(a:Int,b:Int,c:Int,d:Int){
+    var result=a+b+c+d
+    println(result)
+
+}
+fun myFunfact(text: String){
+    var result=text
+    println(result)
+}
